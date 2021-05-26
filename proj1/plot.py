1fd52689c6abf28c2d6bb22daf144e323078ce26
AbedShabaan <abed.shabaan@epfl.ch>
2021-05-26 18:35:27 +0200

    Modified plot code to include numbers

--- proj1/plot.py
+++ proj1/plot.py
@@ -1,18 +1,19 @@
 import matplotlib.pyplot as plt
-
+import numpy as np
 with open('results.txt', 'r') as f:
     lines = f.readlines()
     means = eval(lines[1])
     stds = eval(lines[3])
 
-ticks = ['Vanilla', 'Vanilla optimized', 'Resnet', 'Siamese', 'Resnet auxiliary', 'Siamese auxiliary', 'Siamese semi auxiliary']
-plt.bar(x=ticks, height=means.values())
-for i, (k, v) in enumerate(stds.items()):
-    plt.plot([ticks[i],ticks[i]], [means[k]+v, means[k]-v], c='k', linewidth=2)
-plt.xticks(ticks,rotation=45)
+
+plt.bar(x=means.keys(), height=means.values())
+for k, v in stds.items():
+    plt.plot([k,k], [means[k]+v, means[k]-v], c='k', linewidth=2)
+    plt.text(k, means[k]+2e-2, f'{means[k]:.2f}')
+plt.xticks(rotation=45)
 plt.title('Model performances')
 plt.ylabel('Average test accuracy')
+plt.yticks(np.linspace(0,1,11))
 plt.tight_layout()
 plt.show()
 
-b = {}
