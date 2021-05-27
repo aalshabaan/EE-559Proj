36c29dd68a2138c79039b44b6782fc10ade03ff6
AbedShabaan <abed.shabaan@epfl.ch>
2021-05-27 09:49:14 +0200

    Modified plot code to include numbers
    Finished documenting nn

--- Proj2/nn.py
+++ Proj2/nn.py
@@ -52,7 +52,7 @@ class Linear(Module):
         """
         Perform forward pass
         :param x: input tensors
-        :return: result of forward pass
+        :return: result of forward pass, an affine transformation of the input
         """
         self.x = x.clone()
         if self.bias:
@@ -66,7 +66,9 @@ class Linear(Module):
     
     def backward(self, dl_ds:Tensor):
         """
-        Perform backward pass
+        Perform a back pass on the linear layer, accumulating the loss gradient with respect to the parameters
+        :param dl_ds: Tensor, the loss gradient with respect to the layer's output
+        :return: Tensor, the loss gradient with respect to the layer's input
         """
         # dl_db = dl_ds (sum over all samples in batch)
         # dl_dw = dl_ds @ x^(l-1).T (sum over samples in batch)
@@ -93,6 +95,9 @@ class Linear(Module):
 
     
     def zero_grad(self):
+        """
+        Resets the module's accumulated gradient to zero, preparing for a new round of updates
+        """
         self.dl_dw.fill_(0)
         if self.bias:
             self.dl_db.fill_(0)
@@ -102,31 +107,47 @@ class Linear(Module):
 
 class Sequential(Module):
     
-    def __init__(self, *modules):
+    def __init__(self, *modules: Module):
+        """
+        Module allowing the combination of multiple other modules in a simple sequential architecture
+        :param modules: The modules to be combined into the model ordered from first to last, that's to say input to output
+        """
         super(Module, self).__init__()
         self.module_list = list(modules)
 
-    def forward(self, input_):
+    def forward(self, input_: Tensor):
+        """
+        Performs a forward pass of the sequential model by doing forward passes of all the encompassed modules
+        :param input_: Tensor: The input tensor to the model
+        :return: The output of the model
+        """
         output = input_
-        #print('FORWARD')
         for m in self.module_list:
-            #print(output.shape)
             output = m.forward(output)
         return output
 
-    def backward(self, gradwrtoutput):
-        #print('BACK')
+    def backward(self, gradwrtoutput: Tensor):
+        """
+        Performs a backward pass of the model by performing backward passes on all the modules from end to start
+        :param gradwrtoutput: Tensor: The loss gradient with respect to the model's output
+        :return: Tensor: The loss gradient with respect to the model's input
+        """
         grad = gradwrtoutput
         for i in range(len(self.module_list)-1, 0, -1):
-            #print(type(self.module_list[i]))
-            #print("gradient shape", grad.shape)
             grad = self.module_list[i].backward(grad)
         return grad
 
     def param(self):
+        """
+        Parameters of all the model's module parameters
+        :return: List of lists of pairs corresponding to the model's modules' parameters and gradients
+        """
         return [m.param() for m in self.module_list]
 
     def zero_grad(self):
+        """
+        Resets the gradients of all the model's modules
+        """
         [m.zero_grad() for m in self.module_list]
     
     
@@ -134,16 +155,33 @@ class Sequential(Module):
 class ReLU(Module):
     
     def __init__(self):
+        """
+        Module representing a ReLU activation function, keeps the positive input unchanged and sets the output 0 for negative input
+        """
         super().__init__()
-        
+
     def forward(self, x:Tensor):
-        self.grad = x.gt(0)
+        """
+        Forward pass of the ReLU module. Keeps the positive input unchanged and sets the output 0 for negative input
+        :param x: Tensor: input tensor
+        :return: Tensor, output tensor
+        """
+        self.grad = x.gt(0) # No need to keep track of the input value, only if it's positive or negative
         return x.relu()
 
     def backward(self, dl_dx:Tensor):
+        """
+        Backward pass of the ReLU module, sets the gradient to 0 where ReLU's input is negative
+        :param dl_dx: Tensor: loss gradient with respect to the output
+        :return: Tensor: loss gradient with respect to the input
+        """
         return self.grad*dl_dx
 
     def param(self):
+        """
+        returns an empty list, here to facilitate the inclusion of ReLU in a sequential model
+        :return: empty list
+        """
         return []
 
     def zero_grad(self):
@@ -154,16 +192,33 @@ class ReLU(Module):
 class Tanh(Module):
     
     def __init__(self):
+        """
+        Module representing the tanh output function
+        """
         super().__init__()
         
     def forward(self, x:Tensor):
-        self.x = x
+        """
+        Pass the input through a tanh function component-wise
+        :param x: Tensor: Input tensor
+        :return: Tensor: output tensor
+        """
+        self.x = x # We keep track of the input to calculate the module's gradient
         return x.tanh()
 
     def backward(self, dl_dx:Tensor):
+        """
+        Backward pass of the tanh module
+        :param dl_dx: Tensor: loss gradient with respect to the output
+        :return: Tensor: loss gradient with respect to the input
+        """
         return dl_dx*(1-self.x.tanh().pow(2))
 
     def param(self):
+        """
+        returns an empty list, here to facilitate the inclusion of tanh in a sequential model
+        :return: empty list
+        """
         return []
 
     def zero_grad(self):
@@ -171,15 +226,16 @@ class Tanh(Module):
     
     
 class LossMSE(Module):
-    """
-    Class of MSE loss
-    """
+
     def __init__(self):
+        """
+        Module representing the MSE Loss
+        """
         super().__init__()
     
     def forward(self, pred:Tensor, target:Tensor):
         """
-        Perform forward pass
+        Perform forward pass of MSE Loss
         :param pred: predicted value
         :param target: target value
         :return: result of forward pass
@@ -189,16 +245,11 @@ class LossMSE(Module):
         self.target = target
         if len(target.size()) == 1:
             self.target = self.target.unsqueeze(1)
-#         return (self.pred - self.target).pow(2).mean()
         return (self.pred - self.target).pow(2).mean()
     
     def backward(self):
         """
         Perform backward pass
-        :return: gradient of the loss with respect to predicted values
+        :return: gradient of the loss with respect to the predicted values
         """
-        #print('MSE')
-        #print(self.pred.shape)
-        #print(self.target.shape)
         return 2 * (self.pred - self.target) / (self.target.size(0))
-#         return 2 * (self.pred - self.target) / (self.target.size()[0] * self.target.size()[1])
--- proj1/plot.py
+++ proj1/plot.py
@@ -6,14 +6,14 @@ with open('results.txt', 'r') as f:
     stds = eval(lines[3])
 
 
-plt.bar(x=means.keys(), height=means.values())
+plt.bar(x=means.keys(), height=[100*x  for x in means.values()])
 for k, v in stds.items():
-    plt.plot([k,k], [means[k]+v, means[k]-v], c='k', linewidth=2)
-    plt.text(k, means[k]+2e-2, f'{means[k]:.2f}')
+    plt.plot([k,k], [(means[k]+v)*100, (means[k]-v)*100], c='k', linewidth=2)
+    plt.text(k, 100*means[k]+2, f'{100*means[k]:.2f}')
 plt.xticks(rotation=45)
 plt.title('Model performances')
-plt.ylabel('Average test accuracy')
-plt.yticks(np.linspace(0,1,11))
+plt.ylabel('Average test accuracy (%)')
+plt.yticks(np.linspace(0,100,11))
 plt.tight_layout()
 plt.show()
 
